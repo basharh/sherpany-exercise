afe645e41df835aa3f4f52051743bb913f714979
Bashar Harfoush <bashar.harfoush@gmail.com>
2017-09-16 21:31:56 -0400

    add react-dropzone and integrate it with upload dialog

--- js/components/Upload.js
+++ js/components/Upload.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import DropZone from 'react-dropzone';
+
+function onDrop(files) {
+  console.log('files dropped:', files);
+}
 
 export default function Upload(props) {
+  let dropzone;
   return (
     <div id="upload">
       <div id="header">
@@ -12,8 +18,12 @@ export default function Upload(props) {
         </div>
       </div>
       <div id="drop">
-        <div className="dropbox">
+        <DropZone onDrop={onDrop} accept="image/*" multiple={false} className="dropbox"
+          ref={(input) => { dropzone = input; }}>
           <i className="fa fa-plus"></i>&nbsp;Drop new documents here
+        </DropZone>
+        <div className="openDialog">
+          or <a onClick={() => dropzone.open()}>Browse your computer</a>
         </div>
       </div>
       <div id="available">
